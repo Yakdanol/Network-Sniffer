3290ed7558e7d9fb041cafd06d94934d7b8122d9
Yakdanol <danilaolegovich@yandex.ru>
2025-01-16 15:09:02 +0300

    Initial commit

---
+++ src/main/java/org/yakdanol/networksniffer/NetworkSnifferApplication.java
@@ -0,0 +1,13 @@
+package org.yakdanol.networksniffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkSnifferApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkSnifferApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/yakdanol/networksniffer/NetworkSnifferApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yakdanol.networksniffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkSnifferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
