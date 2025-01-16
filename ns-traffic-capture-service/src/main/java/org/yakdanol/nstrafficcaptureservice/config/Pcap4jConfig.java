4801e2c1b0e2a86e121bc52023ecd044abb9ee15
Yakdanol <danilaolegovich@yandex.ru>
2025-01-16 18:01:45 +0300

    NSf-1 : Добавил файл Pcap4jConfig для выбора конфигурации сетевого модуля

---
+++ ns-traffic-capture-service/src/main/java/org/yakdanol/nstrafficcaptureservice/config/Pcap4jConfig.java
@@ -0,0 +1,25 @@
+package org.yakdanol.nstrafficcaptureservice.config;
+
+import org.pcap4j.core.PcapNetworkInterface;
+import org.pcap4j.core.Pcaps;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class Pcap4jConfig {
+
+    @Value("${traffic-capture.interface-name}")
+    private String interfaceName;
+
+    @Bean
+    public PcapNetworkInterface networkInterface() throws Exception {
+        List<PcapNetworkInterface> allDevs = Pcaps.findAllDevs();
+        return allDevs.stream()
+                .filter(dev -> dev.getName().equals(interfaceName))
+                .findFirst()
+                .orElseThrow(() -> new Exception("Network interface not found: " + interfaceName));
+    }
+}
