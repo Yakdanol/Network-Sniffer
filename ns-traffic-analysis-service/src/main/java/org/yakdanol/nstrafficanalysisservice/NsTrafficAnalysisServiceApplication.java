304696e282a5f845975ba8f6e7766196a4517e02
Yakdanol <danilaolegovich@yandex.ru>
2025-02-23 17:07:42 +0300

    NSf-2: Создал новый модуль для микросервиса Traffic Analysis

---
+++ ns-traffic-analysis-service/src/main/java/org/yakdanol/nstrafficanalysisservice/NsTrafficAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package org.yakdanol.nstrafficanalysisservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsTrafficAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsTrafficAnalysisServiceApplication.class, args);
+	}
+
+}
