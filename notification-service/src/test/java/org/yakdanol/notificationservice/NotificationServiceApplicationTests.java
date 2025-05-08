5bfed7b5f00cd41463737e8c00f725bb404b66eb
Yakdanol <danilaolegovich@yandex.ru>
2025-05-09 01:59:47 +0300

    NSf-4: Создал новый модуль для микросервиса Notification Service

---
+++ notification-service/src/test/java/org/yakdanol/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yakdanol.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
